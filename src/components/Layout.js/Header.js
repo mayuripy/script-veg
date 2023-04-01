d6fece0addfd0a6718a219155bef25ccccba5493
mayuripy <115979827+mayuripy@users.noreply.github.com>
2023-04-01 23:47:44 +0530

    navit

--- src/components/Layout.js/Header.js
+++ src/components/Layout.js/Header.js
@@ -1,8 +1,69 @@
 import React from 'react'
+import { AppBar, Box, Toolbar, Typography } from "@mui/material"
+import FastfoodIcon from '@mui/icons-material/Fastfood'
+import { Link } from "react-router-dom"
 
 function Header() {
     return ( <
-        div > Header < /div>
+        >
+        <
+        Box / >
+        <
+        AppBar vomponebt = { "nav" }
+        sax = {
+            { bgcolor: "black" } } >
+        <
+        Toolbar >
+
+
+        <
+        Typography color = { 'goldenrod' }
+        variant = 'h6'
+        component = "div" >
+        <
+        FastfoodIcon / >
+        My Restrurent <
+        /Typography> <
+        /Toolbar> <
+        Box sx = {
+            { displayL: { xs: "none", sm: "block" } } } >
+        <
+        ul classname = "navigation-menu" >
+        <
+        li to = { "./" } > Home < /li> <
+        /ul> <
+        ul >
+        <
+        li to = { "../menu" } > Menu < /li> <
+        /ul> <
+        ul >
+        <
+        li to = { "../about" } > About < /li> <
+        /ul> <
+        ul >
+        <
+        li to = { "../contact" } > Contact < /li> <
+        /ul>
+
+        <
+        /Box>
+
+        <
+        /AppBar>
+
+
+
+
+
+
+
+
+
+        <
+        />
+
+
+
     )
 }
 
